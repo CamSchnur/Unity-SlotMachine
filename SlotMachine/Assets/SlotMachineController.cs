ccb4fb8308a4dc47241d5eab2fb65e40da1798dd
Cameron Schnur <camschnur@gmail.com>
2018-08-13 19:21:50 -0700

    create basic wheels, need more work to addtorque or similar for spinning

---
+++ SlotMachine/Assets/SlotMachineController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotMachineController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        //todo: add slot machine handle that is pulled when spin is pressed
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Spin();
+        }
+    }
+    private void Spin()
+    {
+        foreach (Transform child in this.GetComponentsInChildren<Transform>())
+        {
+            WheelScript script = child.gameObject.GetComponent<WheelScript>();
+            if (script != null)
+            {
+                script.Spin();
+            }
+        }
+    }
+}
---
+++ SlotMachine/Assets/WheelScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelScript : MonoBehaviour {
+
+
+    private float spinSpeed = 40;
+    private Rigidbody wheelBody;
+    private float rotDegrees;
+	// Use this for initialization
+	void Start ()
+    {
+        rotDegrees = 0.0f;
+        wheelBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+     //   Spin();
+	}
+
+    private void FixedUpdate()
+    {
+       
+    }
+    public void Spin()
+    {
+        //roughly 30-50 is a good value for torque
+        spinSpeed = (Random.value * 20) + 30;
+
+        wheelBody.AddTorque(spinSpeed, 0, 0);
+      // wheelBody.transform.Rotate(0)
+
+    }
+}
